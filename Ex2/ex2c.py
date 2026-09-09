d65fd099ba377b07656deea85ec1b359ce76edc4
Muthukumaran R <91403552+kumaran-57@users.noreply.github.com>
2026-09-09 07:59:44 +0530

    Delete Ex2 directory

--- Ex2/ex2c.py
+++
@@ -1,9 +0,0 @@
-def main():
-    year = int(input("Enter the Year (YYYY) : "))
-    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-        print(f"\nThe Given year {year} is a Leap Year")
-    else:
-        print(f"\nThe Given year {year} is Not a Leap Year")
-
-if __name__ == "__main__":
-    main()
